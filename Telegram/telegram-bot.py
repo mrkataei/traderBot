6eb4f001de0a4ae63bd724e65a2516f3bee8e975
kouroshinfo96@gmail.com <kouroshinfo96@gmail.com>
2021-07-29 01:52:37 +0430

    telegram bot updated and bugs fix

--- Telegram/telegram-bot.py
+++ Telegram/telegram-bot.py
@@ -3,33 +3,56 @@ import telebot
 from time import sleep
 from binance.client import Client
 import pandas as pd
+
 client = Client()
 symbol = []
 # API_KEY = os.getenv('API_KEY')
 API_KEY = '1936293973:AAFLKY0TCP9qEMjqPDrewsdzGisNSQmB0ds'
 bot = telebot.TeleBot(API_KEY)
 users = []
+
+
+def check_users(username: str):
+    if users:  # first init
+        for user in users:
+            if user == username:
+                return False
+            else:
+                return True
+    else:
+        return True
+
+
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_chat_action(chat_id=message.chat.id ,  action="typing")
-    users.append(message.chat.username)
+    bot.send_chat_action(chat_id=message.chat.id, action="typing")
+
     sleep(1)
-    bot.reply_to(message, "Hey " + message.chat.first_name + "!\n"+
+    bot.reply_to(message, "Hey " + message.chat.first_name + "!\n" +
                  "I am Aran , your trade assistance \n"
-                 "/help show commands" )
-    #send notif to admin
-    print(users)
-    if message.chat.username:
-        bot.send_message(chat_id=1210507821 , text="Hi Admin!\n" + "@"+message.chat.username + " just join now" )
-    else:
-        bot.send_message(chat_id=1210507821 , text="Hi Admin!\n" +message.chat.first_name + " just join now" )
+                 "/help show commands")
+    # send notif to admin
+    try:
+        if message.chat.username:
+            users.append(message.chat.username) if check_users(message.chat.username) else print("user already exist")
+            bot.send_message(chat_id=1210507821, text="Hi Admin!\n" + "@" + message.chat.username + " just join now")
+        else:
+            users.append("N.U." + message.chat.first_name) if check_users(message.chat.first_name) else print(
+                "user already exist")
+            bot.send_message(chat_id=1210507821, text="Hi Admin!\n" + message.chat.first_name + " just join now")
+        print(users)
+    except:
+        print("Unblock Bot Admin 😐")
+
 
 @bot.message_handler(commands=['help'])
 def help_me(message):
-    bot.send_chat_action(chat_id=message.chat.id ,  action="typing")
+    bot.send_chat_action(chat_id=message.chat.id, action="typing")
     sleep(1)
     bot.reply_to(message, "price <symbol> show last price of symbol\n"
-                          "candle <symbol> show last 1min candle of symbol" )
+                          "candle <symbol> show last 1min candle of symbol")
+
+
 # @bot.message_handler(commands=['test'])
 # def start_message(message):
 #     markup = telebot.types.InlineKeyboardMarkup()
@@ -50,16 +73,17 @@ def help_me(message):
 #     bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id)
 def stock_request(message):
     request = message.text.split()
-    if len(request) < 2 or request[0].lower() not in "price" :
+    if len(request) < 2 or request[0].lower() not in "price":
         return False
     else:
         return True
+
 
 @bot.message_handler(func=stock_request)
 def send_price(message):
     request = str(message.text.split()[1]).upper()
     try:
-        data  = client.get_symbol_ticker(symbol=request)["price"]
+        data = client.get_symbol_ticker(symbol=request)["price"]
         if data:
             bot.send_message(message.chat.id, data)
         else:
@@ -70,30 +94,35 @@ def send_price(message):
 
 def candle_request(message):
     request = message.text.split()
-    if len(request) < 2 or request[0].lower() not in "candle" :
+    if len(request) < 2 or request[0].lower() not in "candle":
         return False
     else:
         return True
+
 
 @bot.message_handler(func=candle_request)
 def send_candle(message):
     request = str(message.text.split()[1]).upper()
 
     try:
-        data = pd.DataFrame(client.get_klines(symbol=request , interval=client.KLINE_INTERVAL_1MINUTE , limit= 1 )).values
-        time = pd.to_datetime(data[0,0] ,unit='ms' ,yearfirst=True).tz_localize('UTC').tz_convert('Asia/Tehran')
-        if data[0,1] <= data[0,4]:
-            percent  = (float(data[0,4]) - float(data[0,1]) ) * 100 / float(data[0,1])
-            percent  = round(percent, 3)
-            text = "🤑" +request + "    1min"+"\n🟢" +  str(percent) +"%\n"+"Open time : " + str(time) + "\n" + "Open : " + str(data[0,1]) + \
-                   "\n" + "High : " + str(data[0,2]) + "\n"+ "Low : " + str(data[0,3]) + "\n" + "Close : " + str(data[0,4]) + \
-                   "\n" + "Volume : " + str(data[0,5]) + "\n" + "Number of trades : " + str(data[0,8])
+        data = pd.DataFrame(client.get_klines(symbol=request, interval=client.KLINE_INTERVAL_1MINUTE, limit=1)).values
+        time = pd.to_datetime(data[0, 0], unit='ms', yearfirst=True).tz_localize('UTC').tz_convert('Asia/Tehran')
+        if data[0, 1] <= data[0, 4]:
+            percent = (float(data[0, 4]) - float(data[0, 1])) * 100 / float(data[0, 1])
+            percent = round(percent, 3)
+            text = "🤑" + request + "    1min" + "\n🟢" + str(percent) + "%\n" + "Open time : " + str(
+                time) + "\n" + "Open : " + str(data[0, 1]) + \
+                   "\n" + "High : " + str(data[0, 2]) + "\n" + "Low : " + str(data[0, 3]) + "\n" + "Close : " + str(
+                data[0, 4]) + \
+                   "\n" + "Volume : " + str(data[0, 5]) + "\n" + "Number of trades : " + str(data[0, 8])
         else:
-            percent  = (float(data[0,4]) - float(data[0,1]) ) * 100 / float(data[0,1])
-            percent  = round(percent, 3)
-            text = "😰" +request + "    1min"+"\n🔴" +  str(percent) +"%\n" "Open time : " + str(time) + "\n" + "Open : " + str(data[0,1]) + \
-                   "\n" + "High : " + str(data[0,2]) + "\n"+ "Low : " + str(data[0,3]) + "\n" + "Close : " + str(data[0,4]) + \
-                   "\n" + "Volume : " + str(data[0,5]) + "\n" + "Number of trades : " + str(data[0,8])
+            percent = (float(data[0, 4]) - float(data[0, 1])) * 100 / float(data[0, 1])
+            percent = round(percent, 3)
+            text = "😰" + request + "    1min" + "\n🔴" + str(percent) + "%\n" "Open time : " + str(
+                time) + "\n" + "Open : " + str(data[0, 1]) + \
+                   "\n" + "High : " + str(data[0, 2]) + "\n" + "Low : " + str(data[0, 3]) + "\n" + "Close : " + str(
+                data[0, 4]) + \
+                   "\n" + "Volume : " + str(data[0, 5]) + "\n" + "Number of trades : " + str(data[0, 8])
         bot.send_message(message.chat.id, text)
     except:
         bot.send_message(message.chat.id, "invalid symbol or something wrong \n"
@@ -106,7 +135,8 @@ def add_symbol(message):
         symbol.append(message.text.split()[1])
         bot.send_message(message.chat.id, message.text.split()[1] + " added to the coin list")
     except:
-        bot.reply_to(message , "no coin entered")
+        bot.reply_to(message, "no coin entered")
+
 
 @bot.message_handler(commands=['coins'])
 def show_coins(message):
@@ -114,6 +144,7 @@ def show_coins(message):
     for coin in symbol:
         coins += str(coin) + "  "
     bot.send_message(message.chat.id, "coins already watchlist : " + coins)
+
 
 @bot.message_handler(commands=['remove'])
 def remove_coins(message):
@@ -123,10 +154,9 @@ def remove_coins(message):
             symbol.remove(coin)
             bot.reply_to(message, "coin has been removed")
         except:
-            bot.reply_to(message, message.text.split()[1] +  " not in list")
+            bot.reply_to(message, message.text.split()[1] + " not in list")
     except:
         bot.reply_to(message, "no coin entered")
-
 
 
 bot.polling()
