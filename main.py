ee18d84947a1225673fc479b848d0acf510808b0
kourosh <kouroshinfo96@gmail.com>
2021-10-09 11:34:53 +0330

    Revert "bugs fix"
    This reverts commit c5e40e76b906920b69092f470ae9105c2805b38b.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from Analysis.stream import StrategiesThreads
 from Libraries.data_collector import generate_data
 from time import sleep
 
-generate_data()
+generate_data('BTCUSDT', 'ETHUSDT', 'ADAUSDT', 'DOGEUSDT', 'BCHUSDT', 'ETCUSDT')
 thread = StrategiesThreads('BTCUSDT', 'ETHUSDT', 'ADAUSDT', 'DOGEUSDT', 'BCHUSDT', 'ETCUSDT')
 
 if __name__ == '__main__':
